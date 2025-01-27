699e1d989886d2f5116f8ca994683a78a925188c
Jedidiah Carlson <jed.e.carlson@gmail.com>
2025-01-27 13:49:37 -0600

    activity 02

--- add_activity.sh
+++ add_activity.sh
@@ -2,8 +2,8 @@ activity=${1}
 
 cd ../STAT155
 git pull origin main
-cp activities/${activity}*.qmd ../155_fall_2024/activities/
-cp activities/${activity}*.qmd ../155_fall_2024/solutions/
+cp activities/${activity}*.qmd ../155_spring_2025/activities/
+cp activities/${activity}*.qmd ../155_spring_2025/solutions/
 cd ../155_fall_2024/
 
 
